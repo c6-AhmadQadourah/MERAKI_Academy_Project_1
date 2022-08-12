f855d97457995df3e23c1bb9635bc14c5acba087
AhmadQadourah <ahmad.h.qadoura@gmail.com>
2022-08-12 13:50:06 +0300

    aaa

--- Project1.js
+++ Project1.js
@@ -1,36 +1,65 @@
-const body = document.querySelector('body');
+const body = document.querySelector("body");
 
-const div1 = document.createElement( 'div');
-div1.id= 'Div1';
-const ul = document.createElement('lu')
-const but1=document.createElement('button');
-but1.innerText= 'Start '; 
-but1.className = 'but1'
-body.append( but1);
-body.append(div1) ;
-body.append(ul) 
-const Qustions1 = [{ qustion1: ['What is Your Name'] , answer1: ['ahmad ' , 'abdulla' , 'anas ' ] } , {qustion2: [' How old Are You '] , answer2   : [, 30 , 60 , 120 , 1 ] }, {correctanswer:['ahmad' , 30 ] }] 
+const div1 = document.createElement("div");
+div1.id = "Div1";
 
-const li = document.createElement('li')
+const div2 = document.createElement("div");
+const ul = document.createElement("ul");
+const but1 = document.createElement("button");
+but1.innerText = "Start ";
+but1.className = "but1";
+body.append(but1);
+body.append(div1);
+body.append(div2)
+body.append(ul);
 
-ul.append(li) 
- 
+const Qustions1 = [
+  {
+    qustion1: "What is Your Name",
+    answer1: ["ahmad ", "abdulla", "anas ", " mohammad"],
+  },
+  { qustion2: " How old Are You ", answer2: ["30", "60", "120", "1 "] },
+  { correctanswer: ["ahmad", 30] },
+];
 
-const li1 = document.createElement('li')
+// li contains  Q2 + answers + formula for Q2 
+const Q1 = Qustions1[0].qustion1;
+console.log(Q1);
+div2.append(Q1);
 
-ul.append(li1)
- 
-const but= () => {
-    for (let i =0 ; i< Qustions1.length ; i++ ) {
-        const but1 = document.createElement('button') 
-but1.innerText =  Qustions1[i]
-li1.append(but1) 
-ul.append(li1)
+const Q2 = Qustions1[1].qustion2;
+console.log(Q2);
+
+const li = document.createElement("li");
+li.append(Q2);
+ul.append(li);
+
+const li2 = document.createElement("li");
+
+const A1 = Qustions1[0].answer1
+console.log(A1)
+
+
+const but = () => {
+  for (let i = 0; i < 4; i++) {
+    const but1 = document.createElement("button");
+    but1.innerText = Qustions1[1].answer2[i];
+    li.append(but1);
+    ul.append(li);
+    div1.append(li);
+  }
+};
+
+but();
+
+const but2= () => {
+    for (let i =0 ; i< 4 ; i++ ) {
+        const but2 = document.createElement('button') 
+but2.innerText =  A1[i];
+li2.append(but2) 
+ul.append(li2)
+div2.append(li2)
     }
 } 
-but()
- 
-
-
-
+but2()
 
